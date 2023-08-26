c0e8284273eb9ec5e4a20ff336345bee13cf1f65
Olga <helga_ser@mail.ru>
2023-08-26 20:52:36 +0300

    DZ11 Initial

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -3,6 +3,16 @@ package ru.netology.javaqa;
 public class Radio {
     private int radioStation; // номер радиостанции
     private int soundVolume; // уровень громкости звука
+    private int maxRadioStation; // максимальное кол-во радиостанций
+    private int maxSoundVolume = 100; // максимальное значение звука
+
+    public Radio(int maxRadioStation) {
+        this.maxRadioStation = maxRadioStation;
+    }
+
+    public Radio() {
+        this.maxRadioStation = 10;
+    }
 
     public int getRadioStation() {
         return radioStation;
@@ -12,12 +22,16 @@ public class Radio {
         return soundVolume;
     }
 
+    public int getMaxRadioStation() {
+        return maxRadioStation;
+    }
+
     public void setSoundVolume(int newSoundVolume) {
         if (newSoundVolume < 0) {
             return;
         }
-        if (newSoundVolume > 100) {
-            soundVolume = 100;
+        if (newSoundVolume > maxSoundVolume) {
+            soundVolume = maxSoundVolume;
             return;
         }
         soundVolume = newSoundVolume;
@@ -27,41 +41,41 @@ public class Radio {
         if (newRadioStation < 0) {
             return;
         }
-        if (newRadioStation > 9) {
-            radioStation = 9;
+        if (newRadioStation >= maxRadioStation) {
+            radioStation = maxRadioStation - 1;
             return;
         }
         radioStation = newRadioStation;
     }
 
     public void increaseVolume() {
-        if (soundVolume >= 100) {
+        if (soundVolume >= maxSoundVolume) {
             return;
         }
-        soundVolume = soundVolume + 1;
+        soundVolume++;
     }
 
     public void decreaseVolume() {
         if (soundVolume <= 0) {
             return;
         }
-        soundVolume = soundVolume - 1;
+        soundVolume--;
     }
 
     public void increaseRadioStation() {
-        if (radioStation == 9) {
+        if (radioStation == (maxRadioStation - 1)) {
             radioStation = 0;
             return;
         }
-        radioStation = radioStation + 1;
+        radioStation++;
     }
 
     public void decreaseRadioStation() {
         if (radioStation == 0) {
-            radioStation = 9;
+            radioStation = maxRadioStation - 1;
             return;
         }
-        radioStation = radioStation - 1;
+        radioStation--;
     }
 
 }
